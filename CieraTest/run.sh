e6e741fc21dd75730070e42cac8bc4bdb8494c04
amullarney <alasdar@sbcglobal.net>
2021-09-07 14:10:20 -0700

    More tests
    Some faulty code gen.

---
+++ CieraTest/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp $HOME/.m2/repository/io/ciera/runtime/2.5.1-SNAPSHOT/runtime-2.5.1-SNAPSHOT.jar:$HOME/.m2/repository/org/antlr/antlr4-runtime/4.9.1/antlr4-runtime-4.9.1.jar:$HOME/.m2/repository/org/xtuml/CieraTest/1.0.0-SNAPSHOT/CieraTest-1.0.0-SNAPSHOT.jar cieratest.CieraTestApplication
